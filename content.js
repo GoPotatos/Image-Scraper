f266ff054d354ce86d5e34c08d8b0ef2d4238fcb
GoPotatos <awabelmahe@gmail.com>
2020-12-04 17:59:50 +0200

    Capitalize

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener((msg,msg2,sendRespose)=>{
     //console.log(msg,msg2);
     const maxValueElement=document.querySelector('[data-testid="slider-handle"]');
     let maxValue=maxValueElement?maxValueElement.getAttribute("aria-valuemax"):0;
-    const title=location.pathname.slice(1);
+    const title=location.pathname.slice(1).replace(/\b./g,s=>s.toUpperCase());
     const response={
         maxValue,
         src:document.querySelector('[data-testid="product-detail-image"]').src,
